95515f791a76fc88174e7ffa05f063c16174b603
Huy Dang <dangokc@gmail.com>
2021-10-26 11:11:04 +0700

    Deleted unused files

--- myAmarisGate/Scripts-SQL/3.Rename tables.sql
+++
@@ -1,8 +0,0 @@
-EXECUTE sp_rename N'dbo.GATE_Material', N'Gate_GenericMaterial', 'OBJECT' 
-GO
-EXECUTE sp_rename N'dbo.Gate_GenericMaterial.MaterialId', N'Tmp_GenericMaterialId', 'COLUMN' 
-GO
-EXECUTE sp_rename N'dbo.Gate_GenericMaterial.Tmp_GenericMaterialId', N'GenericMaterialId', 'COLUMN' 
-GO
-ALTER TABLE dbo.Gate_GenericMaterial SET (LOCK_ESCALATION = TABLE)
-GO
